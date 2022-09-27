563c6ac0c7a830ba9076e61a1371609a50a4c3f1
xpandeyed <lalbiharipandeyg@gmail.com>
2022-09-27 10:46:31 +0530

    UI Almost done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+var image_paths = ['assets/img1.jpeg', 'assets/img2.jpeg', 'assets/img3.jpeg'];
+
 void main() {
   runApp(MyApp());
 }
@@ -15,9 +17,9 @@ class MyApp extends StatelessWidget {
 
 class Home extends StatelessWidget {
   final TextEditingController _textController = TextEditingController();
-  void handleSubmitted(text) async {
 
-  }
+  void handleSubmitted(text) async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +34,10 @@ class Home extends StatelessWidget {
               child: Container(
                 width: 35,
                 color: Colors.blue,
-                child: Icon(Icons.add, color: Colors.white,),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
@@ -43,41 +48,50 @@ class Home extends StatelessWidget {
               child: Container(
                 width: 35,
                 color: Colors.blue,
-                child: Icon(Icons.add_shopping_cart_rounded, color: Colors.white,),
+                child: Icon(
+                  Icons.add_shopping_cart_rounded,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
         ],
       ),
-
       body: SingleChildScrollView(
         child: Container(
-          color: Colors.black54,
+          color: Color(0xFF202120),
           child: Column(
             children: <Widget>[
               Container(
+                margin: EdgeInsets.all(10),
                 padding: EdgeInsets.all(14),
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
+                    color: Color(0xFF01152b),
+                    borderRadius: BorderRadius.circular(10),
                     border: Border.all(color: Colors.amberAccent, width: 2.0)),
                 child: Row(
                   children: [
-                    Icon(Icons.circle),
-                    Expanded( child: Text("Play and Earn Coins")),
-                    Icon(Icons.arrow_circle_right)
+                    Icon(Icons.circle, color: Colors.yellow),
+                    Expanded(child: Text("Play and Earn Coins", style: TextStyle(color: Colors.white),)),
+                    Icon(Icons.arrow_circle_right, color: Colors.white,)
                   ],
                 ),
               ),
-              CarouselSlider.builder(
-                itemCount: 15,
-                options: CarouselOptions(
-                    autoPlay: true,
-                    enlargeCenterPage: false,
-                    autoPlayInterval: Duration(seconds: 2)),
-                itemBuilder:
-                    (BuildContext context, int itemIndex, int pageViewIndex) =>
-                        Container(
-                  child: Image.asset('assets/img1.jpeg'),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                decoration: BoxDecoration(),
+                child: CarouselSlider.builder(
+                  itemCount: 3,
+                  options: CarouselOptions(
+                      autoPlay: true,
+                      enlargeCenterPage: true,
+                      autoPlayInterval: Duration(seconds: 2)),
+                  itemBuilder: (BuildContext context, int itemIndex,
+                          int pageViewIndex) =>
+                      Container(
+                    child: Image.asset(image_paths[itemIndex]),
+                  ),
                 ),
               ),
               Container(
@@ -102,47 +116,68 @@ class Home extends StatelessWidget {
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(left: 20),
+                padding: EdgeInsets.only(left: 10),
                 child: SizedBox(
                   height: 200.0,
                   child: ListView.builder(
                       shrinkWrap: true,
                       scrollDirection: Axis.horizontal,
-                      itemCount: 5,
+                      itemCount: 3,
                       itemBuilder: (BuildContext context, int index) =>
-                          verticalImageContainer('assets/img1.jpeg',
-                              "Car on Sale", "Starting at 5 laks")),
+                          verticalImageContainer(image_paths[index],
+                              "Amazon Prime", "Starting at 100")),
                 ),
               ),
               Container(
-                color: Colors.tealAccent,
-                margin: EdgeInsets.all(20),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                decoration: BoxDecoration(
+                    color: Color(0xFF2a2b2a),
+                    borderRadius: BorderRadius.circular(10)),
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                 child: Row(
                   children: [
                     Image.asset(
-                      'assets/img1.jpeg',
+                      'assets/youtube.png',
                       height: 50.0,
                       width: 50.0,
                     ),
                     Flexible(
                         flex: 1,
                         child: Container(
-                          margin: EdgeInsets.only(left: 20),
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 20, vertical: 0),
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("YouTube Premium", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                              Text("by Bruised Vein"),
-                              Text("5/6 friend sharing")
+                              Text(
+                                "YouTube Premium",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              Text(
+                                "by Bruised Vein",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                "5/6 friend sharing",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              )
                             ],
                           ),
                         )),
                     Flexible(
                       child: Column(
                         children: [
-                          Text("38/User/Month"),
-                          FlatButton(onPressed: (){}, child: Text("Join"),color: Colors.blue,)
+                          Text("38/User/Month", style: TextStyle(color: Colors.white),),
+                          FlatButton(
+                            onPressed: () {},
+                            child: Text("Join"),
+                            color: Colors.blue,
+                          )
                         ],
                       ),
                       flex: 0,
@@ -151,9 +186,25 @@ class Home extends StatelessWidget {
                 ),
               ),
               Container(
+                width: MediaQuery.of(context).size.width,
+                child: Center(
+                  child: Text(
+                    "Show all public groups.",
+                    style: TextStyle(color: Color(0xFF9f9f9e)),
+                  ),
+                ),
                 margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(10),
-                color: Colors.blue,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    border: Border.all(color: Colors.orangeAccent, width: 2)),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: Color(0xFF2a2b2a),
+                    borderRadius: BorderRadius.circular(10)),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.end,
@@ -164,18 +215,52 @@ class Home extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("What do you want us to list next?"),
-                          Text("Suggest us subscription"),
-                          TextField(
-                            decoration: InputDecoration.collapsed(hintText: "Send a message"),
+                          Container(
+                            child: Text(
+                              "What do you want us to list next?",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ),
+                            ),
+                            margin: EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 0),
+                          ),
+                          Container(
+                              child: Text(
+                                "Suggest us subscription",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              margin: EdgeInsets.only(top: 10)),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            decoration: BoxDecoration(
+                                color: Color(0xFF3c3d3c),
+                                borderRadius: BorderRadius.circular(4)),
+                            padding: EdgeInsets.all(10),
+                            child: TextField(
+                              showCursor: true,
+                              decoration: InputDecoration.collapsed(
+                                  hintText: "Send a message"),
+                            ),
                           )
                         ],
                       ),
                     ),
-                    Flexible(flex: 0,child: Icon(Icons.send),)
+                    Flexible(
+                      flex: 0,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
@@ -213,7 +298,7 @@ Widget verticalImageContainer(image, title, subtitle) {
   return AspectRatio(
     aspectRatio: 1 / 1,
     child: Container(
-      margin: EdgeInsets.only(right: 20),
+      margin: EdgeInsets.only(right: 10),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           image: DecorationImage(
@@ -240,7 +325,10 @@ Widget verticalImageContainer(image, title, subtitle) {
                   alignment: Alignment.topLeft,
                   child: Container(
                     width: 50,
-                    child: Text("Upto 70% off"),
+                    child: Text(
+                      "Upto 70% off",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   )),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
